89d1386a20bec1b07b7165592aa0ab42bc0ba03f
Mixas V <63231178+MixasV@users.noreply.github.com>
2022-12-02 20:10:11 +0300

    Update index.js
    Упростил и чуть изменил скрипт, чтобы просто выдавал наши IDs профилей, а так же входил бы по токену (нужно вставить в ручную), так как у меня почему-то возникла проблема с автоматическим получением токена через API Dolphin.

--- index.js
+++ index.js
@@ -47,37 +47,6 @@ const getProfiles = async token => {
     return false
 }
 
-/**
- * Открываем Anty Dolphin. На входе id профиля,
- * на выходе данные для подключения к инстансу
- * @param profileId
- * @returns {Promise<*>}
- */
-const openBrowser = async profileId => {
-    const {data} = await axios(`http://localhost:3001/v1.0/browser_profiles/${profileId}/start?automation=1`)
-    return data.automation
-}
-
-/**
- * Функция автоматизации. Запускает инстанс браузера,
- * после открывает страницу гугла и делает скнишот.
- * На входе id профиля.
- * @param profileId
- * @returns {Promise<void>}
- */
-const automation = async profileId => {
-    const {port, wsEndpoint} = await openBrowser(profileId)
-
-    const browser = await puppeteer.connect({
-        browserWSEndpoint: `ws://127.0.0.1:${port}${wsEndpoint}`
-    });
-
-    const page = await browser.newPage();
-    await page.goto('https://google.com');
-    await page.screenshot({ path: 'google.png' });
-
-    await browser.close()
-}
 
 /**
  * Стартовая функция. Поэтапно:
@@ -86,7 +55,7 @@ const automation = async profileId => {
  * 3. Обходит циклом, запуская функцию автоматизации
  */
 (async _ => {
-    const token = await auth()
+    const token = "<ВСТАВЬТЕ СЮДА ВАШ ТОКЕН>"
     if (!token) {
         console.log('Токен не получен')
         return
@@ -95,9 +64,10 @@ const automation = async profileId => {
     if (!profilesIds) {
         console.log('Профили отсутствуют')
         return
+    }else{
+        console.log(profilesIds)
+    }
     }
 
-    for(let i = 0; i < profilesIds.length; i++) {
-        automation(profilesIds[i])
-    }
-})()
+    
+)()
